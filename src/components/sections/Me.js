9a4e286f86874276a9215d97cdc5d6ae0d13e861
Ryan Knight <ryan.knight09@outlook.com>
2023-04-07 12:37:12 -0700

    fix

--- src/components/sections/Me.js
+++ src/components/sections/Me.js
@@ -97,7 +97,7 @@ const Me = () => {
             I am currently working at a rad startup called GeodeIP, building legal ops software. 
             Software is a silly thing. I enjoy making art out of the front end architecture and 
             code maintainability. Its a fickle thing. I am not all about software however. I love
-            music and recording music. My life long endeavor is building a kick ass recording studio...
+            music and recording music. My life long endeavor is building a professional recording studio...
           </p>
         </div>
         <div>
